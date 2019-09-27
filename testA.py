e6ec5fb137245b21a96a0fc313975d8f87a21f75
PaklongC <pak_long@live.nl>
2019-09-27 12:10:03 +0200

    f

--- testA.py
+++ testA.py
@@ -6,8 +6,8 @@ ser=serial.Serial("/dev/ttyAMA0",9600)  #change ACM number as found from ls /dev
 ser.baudrate=9600
 def blink(pin):
 
-GPIO.output(pin,GPIO.HIGH)
-    time.sleep(1)
+    GPIO.output(pin,GPIO.HIGH)
+time.sleep(1)
 GPIO.output(pin,GPIO.LOW)
     time.sleep(1)
 return
