b6aea7fb4b95a896c44c7d2169f62e3381db1825
Freek Berden <freekberden@gmail.com>
2019-09-30 11:46:34 +0200

    .

--- web/server.py
+++ web/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 app = Flask(__name__)
+sensors = ['sensor1', 'sensor2', 'sensor3']
 
 @app.route('/')
 def hello_world():
@@ -8,7 +9,19 @@ def hello_world():
 @app.route('/home')
 def home():
     return render_template('index.html')
+@app.route('/api/sensors', methods = ['GET'])
+def list():
+    return str(sensors)
+
+@app.route('/api/sensors/<path:sensor_id>', methods = ['GET'])
+def read(sensor_id):
+    global sensors
+    return sensors[sensor_id]
+
+@app.route('/api/sensors', methods = ['POST'])
+def create():
+    sensors.append(request.json["sensorName"])
+    return 'Added sensor!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
- 
