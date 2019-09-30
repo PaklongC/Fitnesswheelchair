89a1c7afbfe8b5450211736d8fdf58c1d171d106
Freek Berden <freekberden@gmail.com>
2019-09-30 13:20:13 +0200

    .

--- web/server2.py
+++ web/server2.py
@@ -21,7 +21,10 @@ def read(sensor_id):
 def create():
     sensors.append(request.json["sensorName"])
     return 'Added sensor!'
-
+    
+@app.route('/home')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
