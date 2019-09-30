7e93214d64147053535b554ec454f9779803a430
lorenzo hoorn <Lorenzo>
2019-09-30 16:13:03 +0200

    updated arduino

--- Arduino/code.ino
+++ Arduino/code.ino
@@ -5,8 +5,8 @@
 #define BNO055_SAMPLERATE_DELAY_MS (100)
 
 Adafruit_BNO055 bno = Adafruit_BNO055();
-unsigned long t0,t1,t2;
-double a, a1, v;
+unsigned long t;
+double a, v, v0;
 
 void setup(void)
 {
@@ -19,26 +19,22 @@ void setup(void)
   }
   delay(1000);
   bno.setExtCrystalUse(true);
-  t0=0;
+  v0=0;
+  t=0.01;
 }
 
 void loop(void)
 {
   imu::Vector<3> accelerometer = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
   a = double (accelerometer.x());
-  t2 = 1000*millis();  //take current time
-  t1 = t2 - t0;    // td = time since last update
-  t0 = t2;        //set last time to current time
-  v = v + a1*t1; //increase or decrease speed
-  a1 = a;          //set new acceleration;
 
-  Serial.print("Time elapsed: ");
-  Serial.println (t1);
+  v = v0 + a*t;
+  v0 = v;
+
   Serial.print("Acceleration measured: ");
   Serial.println (a);
   Serial.print("Calculated speed: ");
   Serial.println(v);
   delay(BNO055_SAMPLERATE_DELAY_MS);
-  delay(3000);
-}
+  delay(10);
 }
