d5ea35855bbafbb555452b77678ef9cb7171b23c
Freek Berden <freekberden@gmail.com>
2019-10-11 11:28:38 +0200

    fuck

--- subscribe_gatt_orientation.py
+++ subscribe_gatt_orientation.py
@@ -35,11 +35,11 @@ ADDRESS_TYPE = pygatt.BLEAddressType.random
 
 
 def handle_orientation_data(handle, value_bytes):
+    try:
     """
     handle -- integer, characteristic read handle the data was received on
     value_bytes -- bytearray, the data returned in the notification
     """
-    try:
     print("Received data: %s (handle %d)" % (str(value_bytes), handle))
     values = [float(x) for x in value_bytes.decode('utf-8').split(",")]
     #find_or_create("Left Wheel Orientation",
