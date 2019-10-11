126744da9fcb0a5cc529cb4388bebeda4043ab55
Freek Berden <freekberden@gmail.com>
2019-10-11 17:48:18 +0200

    .

--- subgat.py
+++ subgat.py
@@ -37,10 +37,17 @@ def handle_orientation_data(handle, value_bytes):
     handle -- integer, characteristic read handle the data was received on
     value_bytes -- bytearray, the data returned in the notification
     """
-    print("Received data: %s (handle %d)" % (str(value_bytes), handle))
-    values = [float(x) for x in value_bytes.decode('utf-8').split(",")]
-    find_or_create("Left Wheel Orientation",
-                   PropertyType.THREE_DIMENSIONS).update_values(values)
+    try:
+        print("Received data: %s (handle %d)" % (str(value_bytes), handle))
+    except:
+        print("Denk het niet")
+    try:
+        values = [float(x) for x in value_bytes.decode('utf-8').split(",")]
+    except:
+        print("Denk het niet2")
+
+    #find_or_create("Left Wheel Orientation",
+                #   PropertyType.THREE_DIMENSIONS).update_values(values)
 
 
 def discover_characteristic(device):
