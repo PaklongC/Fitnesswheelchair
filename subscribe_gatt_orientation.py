058279f5e049f752e229c9fce0e614f5e51e48a5
Freek Berden <freekberden@gmail.com>
2019-10-11 11:17:04 +0200

    as

--- subscribe_gatt_orientation.py
+++ subscribe_gatt_orientation.py
@@ -41,9 +41,9 @@ def handle_orientation_data(handle, value_bytes):
     """
     print("Received data: %s (handle %d)" % (str(value_bytes), handle))
     values = [float(x) for x in value_bytes.decode('utf-8').split(",")]
-    find_or_create("Left Wheel Orientation",
-                   PropertyType.THREE_DIMENSIONS).update_values(values)
-
+    #find_or_create("Left Wheel Orientation",
+    #               PropertyType.THREE_DIMENSIONS).update_values(values)
+    print(values)
 
 def discover_characteristic(device):
     """List characteristics of a device"""
