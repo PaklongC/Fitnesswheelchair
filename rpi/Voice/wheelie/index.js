d8cc338aca9306d4dc19202c3f814c40e90254ed
Freek Berden <freekberden@gmail.com>
2019-10-14 17:02:37 +0200

    .

--- rpi/Voice/wheelie/index.js
+++ rpi/Voice/wheelie/index.js
@@ -2,14 +2,14 @@ const{ withHermes} = require ('hermes-javascript')
 
   withHermes( hermes=>{
     const dialog = hermes.dialog()
-    
+
     dialog.flow('froekoe:data_start',(msg,flow)=>{
       console.log(msg)
       flow.end()
       return "ok lets start running,      oops, start rolling data collection"
     })
 
-    dialog.flow('froekoe:data_start',(msg,flow)=>{
+    dialog.flow('froekoe:data_stop',(msg,flow)=>{
       console.log(msg)
       flow.end()
       return "Stopped data collecting,      start analysis,   you would be faster if you ran"
