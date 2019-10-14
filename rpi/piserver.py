e54157b397d689ed680089f9b3fdd3fcf9f6cb83
Freek Berden <freekberden@gmail.com>
2019-10-14 17:37:09 +0200

    .

--- rpi/piserver.py
+++ rpi/piserver.py
@@ -5,8 +5,10 @@ app = Flask(__name__)
 
 @app.route('/wheelie', methods = ['POST'])
 def create():
-    print('super hacker pro')
+    print('super hacker')
     return 'Added sensor!'
-
+@app.route('/home')
+def home():
+    return render_template('index.html')
 if __name__ == '__main__':
-     app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
