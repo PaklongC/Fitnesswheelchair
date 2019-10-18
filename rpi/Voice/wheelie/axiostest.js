5359742d9f6a355c0ff92c344764da6aeb61ad3a
Freek Berden <freekberden@gmail.com>
2019-10-18 11:31:53 +0200

    .

--- rpi/Voice/wheelie/axiostest.js
+++ rpi/Voice/wheelie/axiostest.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-axios.get('145.94.227.95:5000/wheelie')
+axios.post('145.94.227.95:5000/wheelie')
   .then(response => {
     console.log(response);
     console.log(response.data.url);
