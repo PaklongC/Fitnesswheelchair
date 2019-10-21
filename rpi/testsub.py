3e6fe6271ee93737e74746a34b9c29fe5f55097c
Freek Berden <freekberden@gmail.com>
2019-10-21 15:47:49 +0200

    .

--- rpi/testsub.py
+++ rpi/testsub.py
@@ -1,10 +1,14 @@
 import time
-
-def counter(loop_on):
+i =2
+print('test'+ str(i))
+def counter():
     i = 0
     while i <=10:
-        if loop_on.value == True:
-            i = i + 1
+        i = i + 1
         print(i)
         time.sleep(1)
     print("test")
+def stopfunction():
+    print('stop!')
+print("test213124")
+counter()
