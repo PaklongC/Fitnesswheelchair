b94bb5001ceec2c53ce7569249c87d894a9309bf
Freek Berden <freekberden@gmail.com>
2019-10-21 15:05:38 +0200

    .

--- rpi/testsub.py
+++ rpi/testsub.py
@@ -1,6 +1,10 @@
 import time
-i = 0
-while True:
-        i = i + 1
+
+def counter(loop_on):
+    i = 0
+    while i <=10:
+        if loop_on.value == True:
+            i = i + 1
         print(i)
         time.sleep(1)
+    print("test")
