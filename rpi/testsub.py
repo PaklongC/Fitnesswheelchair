6ce452e1a03ce5efdd85c7bf7180622f15b29283
froekoe <freekberden@gmail.com>
2019-10-22 14:20:48 +0200

    .

--- rpi/testsub.py
+++ rpi/testsub.py
@@ -1,15 +1,33 @@
 import time
-class testclass:
-    i =2
-    print('test'+ str(i))
-    def counter():
-        i = 0
-        while i <=10:
-            i = i + 1
-            print(i)
-            time.sleep(1)
-        print("test")
-    def stopfunction():
-        print('stop!')
-    print("test213124")
-    counter()
+
+def ftest():
+    global s,a,c
+    s = "global test"
+    a = " aaa"
+    c = "cccc"
+def ft2():
+    global s
+    print(s)
+    print(c)
+    s = "local to global"
+ftest()
+print(s)
+ft2()
+print(s)
+print(a)
+'''
+##Test counter###
+i =2
+print('test'+ str(i))
+def counter():
+    i = 0
+    while i <=10:
+        i = i + 1
+        print(i)
+        time.sleep(1)
+    print("test")
+def stopfunction():
+    print('stop!')
+print("test213124")
+counter()
+'''
