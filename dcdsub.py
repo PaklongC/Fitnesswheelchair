54089e880ed69fe3d67f34a1d966defbd412ee76
Freek Berden <freekberden@gmail.com>
2019-10-22 16:16:12 +0200

    .

--- dcdsub.py
+++ dcdsub.py
@@ -61,6 +61,7 @@ def handle_orientation_data(handle, value_bytes):
     try:
         print("Received data: %s (handle %d)" % (str(value_bytes), handle))
         values = [float(x) for x in value_bytes.decode('utf-8').split(",")]
+        values[1]= 3.6*values[1]
     except:
         print("Could not convert data")
     try:
