d41971a68f5826c18d6992b2266e863651441e59
Freek Berden <freekberden@gmail.com>
2019-10-25 11:56:25 +0200

    .

--- rpi/Voice/wheelie/tts.py
+++ rpi/Voice/wheelie/tts.py
@@ -5,7 +5,7 @@ print("creating new instance")
 client = mqtt.Client("SnipsTTS") #create new instance
 print("connecting to broker")
 client.connect(broker_address) #connect to broker
-#print("Subscribing to topic","house/bulbs/bulb1")
-#client.subscribe("house/bulbs/bulb1")
+print("Subscribing to topic","house/bulbs/bulb1")
+client.subscribe("hermes/tts/say")
 print("Publishing message to topic","house/bulbs/bulb1")
 client.publish("hermes/tts/say","Super hacker jwz")
