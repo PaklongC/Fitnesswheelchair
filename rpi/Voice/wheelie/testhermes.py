6b426452ea3a9b4d75a777b43c3e8e768258659e
froekoe <freekberden@gmail.com>
2019-10-27 12:04:28 +0000

    asdasd

---
+++ rpi/Voice/wheelie/testhermes.py
@@ -0,0 +1,5 @@
+from hermes_python.hermes import Hermes, MqttOptions
+
+MQTT_ADDR = " localhost:1883" 
+with Hermes(mqtt_options=MqttOptions()) as h:
+	h.publish_start_session_notification(None,"Super hacker pro",None)
