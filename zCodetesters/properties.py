3123fae899d03657539c6b9c52e3b3a5aa5fe5a3
Freek Berden <freekberden@gmail.com>
2019-11-01 14:34:01 +0100

    .

--- zCodetesters/properties.py
+++ zCodetesters/properties.py
@@ -6,3 +6,17 @@ with open("t.properties") as properties:
 
     print(d['a'])
     properties.close()
+
+with open("config.txt") as properties:
+    l = [line.split("=") for line in properties.readlines()]
+    p = {key.strip(): value.strip() for key, value in l}
+    #global target_distance,target_velocity,deviation_velocity
+    print(p['target_distance'])
+    target_distance = p['target_distance']
+    print(p['target_velocity'])
+    deviation_velocity = p['deviation_velocity']
+        #velocity_min = target_velocity - deviation_velocity
+        #velocity_max = target_velocity + deviation_velocity
+        #pr = "porperties set: " + "TD:"+ target_distance + "TV:" + target_velocity + "DV:" + deviation_velocity
+        #print(pr)
+    properties.close()
