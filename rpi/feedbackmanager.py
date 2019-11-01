c4dd9c68d1785d9f62fdb935d0a567cf8a36d526
Freek Berden <freekberden@gmail.com>
2019-11-01 15:20:11 +0100

    .

--- rpi/feedbackmanager.py
+++ rpi/feedbackmanager.py
@@ -15,20 +15,20 @@ ltime_update = time()
 ltime_positive = time()
 ltime_slow = time() + 10
 lines_slow = ["try going faster", "You are a bit to slow", "go faster", "go go go","Atleast you are not standing still but try harder though your too slow","are you even moving","are you even trying","I dont think you are trying","you are not a good roll model"]
-try:
-    with open("config.txt") as properties:
-        l = [line.split("=") for line in properties.readlines()]
-        p = {key.strip(): value.strip() for key, value in l}
-        #global target_distance,target_velocity,deviation_velocity
-        print(p['target_distance'])
-        target_distance = int(p['target_distance'])
-        target_velocity= int(p['target_velocity'])
-        deviation_velocity = int(p['deviation_velocity'])
-        #velocity_min = target_velocity - deviation_velocity
-        #velocity_max = target_velocity + deviation_velocity
-        properties.close()
-except:
-    print("config werkt niet")
+#try:
+with open("config.txt") as properties:
+    l = [line.split("=") for line in properties.readlines()]
+    p = {key.strip(): value.strip() for key, value in l}
+    #global target_distance,target_velocity,deviation_velocity
+    print(p['target_distance'])
+    target_distance = int(p['target_distance'])
+    target_velocity= int(p['target_velocity'])
+    deviation_velocity = int(p['deviation_velocity'])
+    #velocity_min = target_velocity - deviation_velocity
+    #velocity_max = target_velocity + deviation_velocity
+    properties.close()
+#except:
+#    print("config werkt niet")
 def update(_values):
     global distance,velocity
     distance = _values[3]
