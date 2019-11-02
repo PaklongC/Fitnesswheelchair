55723245d9ee35ebf67a39071bfce8ca38e0515e
froekoe <freekberden@gmail.com>
2019-11-02 17:19:59 +0100

    .

--- zCodetesters/properties.py
+++ zCodetesters/properties.py
@@ -1,3 +1,22 @@
+from pathlib import Path
+mypath = str(Path().absolute())
+print(mypath)
+#def getConfigs():
+with open("configsx.txt") as properties:
+    l = [line.split("=") for line in properties.readlines()]
+    p = {key.strip(): value.strip() for key, value in l}
+    global target_distance,target_velocity,deviation_velocity, velocity_min, velocity_max
+    #print(p['target_distance'])
+    target_distance = int(p['target_distance'])
+    target_velocity = int(p['target_velocity'])
+    deviation_velocity = float(p['deviation_velocity'])
+    velocity_min = target_velocity - deviation_velocity
+    velocity_max = target_velocity + deviation_velocity
+    print(p)
+    properties.close()
+    #return p
+#getConfigs()
+'''
 with open("t.properties") as properties:
     l = [line.split("=") for line in properties.readlines()]
     d = {key.strip(): value.strip() for key, value in l}
@@ -20,3 +39,4 @@ with open("config.txt") as properties:
         #pr = "porperties set: " + "TD:"+ target_distance + "TV:" + target_velocity + "DV:" + deviation_velocity
         #print(pr)
     properties.close()
+'''
