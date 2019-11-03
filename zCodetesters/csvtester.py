7a3f3326eb5c0d391ab64fd0abce565fd93d5d4b
froekoe <freekberden@gmail.com>
2019-11-03 15:54:48 +0100

    .

--- zCodetesters/csvtester.py
+++ zCodetesters/csvtester.py
@@ -3,13 +3,13 @@ from pathlib import Path
 
 csvpath = Path(__file__).parent.absolute().joinpath('testFolder').joinpath('tesp.csv')
 print(csvpath)
-try:
-    with open(csvpath, 'a') as csvFile:
-        writer = csv.writer(csvFile)
-        writer.writerow([3,2,1,0])
-        csvFile.close()
-except:
-    print("could not write to csv1")
+
+with open(csvpath, 'a') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerow([3,2,1,0])
+    csvFile.close()
+
+print("could not write to csv1")
 try:
     with open('test2.csv', 'a') as csvFile:
         writer = csv.writer(csvFile)
