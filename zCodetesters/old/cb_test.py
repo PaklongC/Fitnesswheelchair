a92be47929e1b44649d4a716c5fda3b065db90f1
Freek Berden <freekberden@gmail.com>
2019-11-08 12:21:09 +0100

    THE GREAT CLEANUP

--- zCodetesters/old/cb_test.py
+++
@@ -1,9 +0,0 @@
-import time
-import callbacks
-list = [["1","2","3"],["4","5",'6']]
-print(list)
-pr=list[1]
-print(pr[2])
-pr=str(time.strftime("%d_%m_%H%M%S", time.gmtime()))+'.csv'
-cb = callbacks
-print(cb.test)
--- zCodetesters/old/time.py
+++
@@ -1,5 +0,0 @@
-import datetime
-
-print(datetime.datetime.now())
-x = [datetime.datetime.now(),0,0]
-print(x)
