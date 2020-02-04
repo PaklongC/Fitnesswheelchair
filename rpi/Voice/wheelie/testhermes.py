c4ab094696897433633662998d40c543a774d113
PaklongC <55391635+PaklongC@users.noreply.github.com>
2020-02-04 18:02:01 +0100

    MQTT

--- rpi/Voice/wheelie/testhermes.py
+++ rpi/Voice/wheelie/testhermes.py
@@ -2,4 +2,4 @@ from hermes_python.hermes import Hermes, MqttOptions
 
 MQTT_ADDR = " localhost:1883" 
 with Hermes(mqtt_options=MqttOptions()) as h:
-	h.publish_start_session_notification(None,"Super hacker pro",None)
+	h.publish_start_session_notification(None,"Super hacker pro",None) 
